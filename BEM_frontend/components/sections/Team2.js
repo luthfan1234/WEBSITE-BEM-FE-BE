8972e067806ce0a6711eeb13aa7c2dda686eeb47
_umannn <134380467+luthfan1234@users.noreply.github.com>
2025-06-20 19:14:32 +0700

    add layanan

--- BEM_frontend/components/sections/Team2.js
+++ BEM_frontend/components/sections/Team2.js
@@ -1,24 +1,56 @@
-
-import TeamSlider from "../slider/TeamSlider"
+import Link from 'next/link';
 
 export default function Team2() {
-    return (
-        <>
+    const services = [
+        { 
+            name: "LAPOR IN", 
+            image: "/assets/images/lapor.png", 
+            href: "https://uns.id/Hotline_Ruang_Aman_SV",
+            description: "Platform pelaporan aspirasi dan keluhan mahasiswa langsung ke Dekan. Sampaikan suaramu untuk perubahan kampus yang lebih baik." 
+        },
+        { 
+            name: "HOTLINE RUANG AMAN", 
+            image: "/assets/images/hotline.png", 
+            href: "https://uns.id/Hotline_Ruang_Aman_SV",
+            description: "Layanan untuk melaporkan keluhan terkait ruang aman." 
+        },
+    ];
 
-            <section className="tf-section team">
-                <div className="tf-container">
-                    <div className="row justify-content-center">
-                        <div className="col-md-12 ">
-                            <div className="tf-heading mb60 wow fadeInUp">
-                                <h2 className="heading">OUR TEAM</h2>
-                            </div>
-                        </div>
-                        <div className="col-md-12 wow fadeInUp">
-                            <TeamSlider />
+    return (
+        <section className="tf-section team tf-team-ss">
+            <div className="icon">
+                {/* SVG code stays the same */}
+            </div>
+            <div className="tf-container">
+                <div className="row justify-content-center">
+                    <div className="col-md-12">
+                        <div className="tf-heading mb60 wow fadeInUp">
+                            <h2 className="heading">LAYANAN KELUHAN</h2>
+                            <p>Silakan pilih layanan keluhan yang ingin Anda laporkan.</p>
                         </div>
                     </div>
+                    
+                    {services.map((service, index) => (
+                        <div key={index} className="col-lg-3 col-md-3 col-sm-5 col-12">
+                            <Link 
+                                href={service.href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="tf-team kementerian-item"
+                                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}
+                            >
+                                <div className="image" style={{ borderRadius: "0" }}>
+                                    <img src={service.image} alt={service.name} />
+                                </div>
+                                <h4 className="name">
+                                    {service.name}
+                                </h4>
+                                <p>{service.description}</p>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
-            </section>
-        </>
-    )
+            </div>
+        </section>
+    );
 }
